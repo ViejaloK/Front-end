6fc4561aedd9036b8196d0a19e4e836e4523cdf7
maurobossio <maurobossio@outlook.com.ar>
2022-12-22 20:01:17 -0300

    termine pagina de tienda-online (arregle su footer)

--- assets/script.js
+++ assets/script.js
@@ -3,5 +3,6 @@ const nav = document.getElementById('nav-inactive');
 
 menubar.addEventListener('click',() => {
     nav.classList.toggle("nav-active");
+    menubar.classList.toggle("hamburguesa-ico-active");
 });
 
